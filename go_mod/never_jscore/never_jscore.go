adcbd990d236a90ed15f8557391922e2aa4d0a2b
gtsigner <zhaojunlike@gmail.com>
2025-11-21 11:42:30 +0800

    feat: go build

--- go_mod/never_jscore/never_jscore.go
+++ go_mod/never_jscore/never_jscore.go
@@ -1,7 +1,11 @@
 package never_jscore
 
 /*
-#cgo LDFLAGS: -L../../target/release -lnever_jscore
+#cgo darwin,arm64  LDFLAGS: -L${SRCDIR}/lib/aarch64-apple-darwin -lnever_jscore -framework Security -framework SystemConfiguration -framework CoreFoundation
+#cgo darwin,amd64  LDFLAGS: -L${SRCDIR}/lib/x86_64-apple-darwin -lnever_jscore -framework Security -framework SystemConfiguration -framework CoreFoundation
+#cgo linux,amd64  LDFLAGS: -L${SRCDIR}/lib/x86_64-unknown-linux-gnu -lnever_jscore -lrt -lpthread -ldl
+#cgo linux,arm64  LDFLAGS: -L${SRCDIR}/lib/aarch64-unknown-linux-gnu -lnever_jscore -lrt -lpthread -ldl
+#cgo windows,amd64 LDFLAGS: -L${SRCDIR}/lib/x86_64-pc-windows-msvc -lnever_jscore
 #include <stdlib.h>
 
 typedef void* ContextPtr;
